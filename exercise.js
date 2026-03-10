b38baf5a5e607b2b491de1278b65d9158cbbab53
sruthika <n.sruthika24@gmail.com>
2026-03-10 15:54:49 +0530

    tasks
    Signed-off-by: sruthika <n.sruthika24@gmail.com>

---
+++ exercise.js
@@ -0,0 +1,49 @@
+// first exercise
+// const add=(a,b)=>{
+//     return a+b;
+// }
+// console.log(add(7,8));
+
+
+// 2nd
+// function square(n){
+//     console.log(n*n);
+// }
+// square(6);
+
+
+// 3rd
+// const print=()=>{
+//     console.log("hello world");
+// }
+// print();
+
+// // 4th
+// const display=(name)=>{
+//     console.log("HI" + name);    
+// }
+// display("sruthi");
+
+// 5th
+const multiply=(x,y)=> x*y;
+console.log("Multiplication:"+multiply(5,8));
+
+// 6th
+// let arr=[3,5,7,9,6];
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+
+//7th 
+// let arr=[10,20,30,40,50];
+// for (let y of arr){
+//     console.log(y)
+// } 
+
+// 8th
+// let arr=[10,20,30,40,50];
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log("Element:" +arr[i] , " index:"+i);
+// }
