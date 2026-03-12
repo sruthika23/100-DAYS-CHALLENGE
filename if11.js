544b68c448bcf93b8c8f1d8930cbc16575a897d1
sruthika <n.sruthika24@gmail.com>
2026-03-12 13:01:19 +0530

    if statement(11/03/2026)

---
+++ if11.js
@@ -0,0 +1,13 @@
+let mark=60;
+if(mark>=90){
+console.log("GRADE A");
+}
+else if(mark>=60){
+console.log("GRADE B");
+}
+else if(mark>=45){
+console.log("GRADE C");
+}
+else{
+    console.log("Fail");
+}
